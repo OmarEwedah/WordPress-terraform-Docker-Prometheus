2ca2cb9f5e81959f3f6b9aaa7cf04381c00740b2
Omar Ewedah <omar.ewedah@orange.com>
2018-03-21 19:05:28 +0000

    create instance t2.medium

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,5 +1,15 @@
 provider "aws" {
   access_key = "xxxxxxxxxxxxxxxxxxx"
   secret_key = "xxxxxxxxxxxxxxxxx"
-  region = "eu-west-3"
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-5ce55321"
+  instance_type = "t2.medium"
+  
+  tags {
+    Name = "blogging app"
+  }
+
 }
