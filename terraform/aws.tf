9fc8aeffbc7c5220b2935cbed786b26988e0769a
Omar Ewedah <omar.ewedah@orange.com>
2018-03-21 19:34:27 +0000

    create key pair

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -7,9 +7,18 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami           = "ami-5ce55321"
   instance_type = "t2.medium"
+  key_name = "cakesolutions"
   
   tags {
     Name = "blogging app"
   }
-
+    root_block_device {
+    volume_size = "50"
+  }
 }
+
+resource "aws_key_pair" "terraform_ec2_key" {
+  key_name = "cakesolutions"
+  public_key = "ssh-rsa xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
+
