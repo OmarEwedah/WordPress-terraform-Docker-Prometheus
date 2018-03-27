8c28e8c7eaf8a238856450e1422caf7f81349185
Omar Ewedah <oewedah.ext@orange.com>
2018-03-27 12:11:38 +0200

    make nginx config file be localhost

--- nginx/launch.sh
+++ nginx/launch.sh
@@ -1,4 +1,4 @@
 #!bin/bash
-IP_ADDRESS=$(curl -s ifconfig.co)
+#IP_ADDRESS=$(curl -s ifconfig.co)
 
-envsubst $IP_ADDRESS < /etc/nginx/conf.d/nginx.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+envsubst  < /etc/nginx/conf.d/nginx.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
