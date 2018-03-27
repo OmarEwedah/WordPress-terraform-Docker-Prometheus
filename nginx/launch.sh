795b7cc34566e8f65dea3c973c121fa871cb567c
Omar Ewedah <oewedah.ext@orange.com>
2018-03-27 10:59:12 +0200

    change the env variable inside launch script

--- nginx/launch.sh
+++ nginx/launch.sh
@@ -1,2 +1,4 @@
-#!bin/bash 
-envsubst < /etc/nginx/conf.d/nginx.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+#!bin/bash
+IP_ADDRESS=$(curl -s ifconfig.co)
+
+envsubst $IP_ADDRESS < /etc/nginx/conf.d/nginx.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
--- terraform/aws.tf
+++ terraform/aws.tf
@@ -136,41 +136,41 @@ resource "aws_instance" "ec2" {
   command = "sleep 100"
   }
   provisioner "local-exec" {
-  command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/cloud/WordPress-terraform-Docker-Prometheus/ansible/docker.yml --tags docker1,prometheus1,nginx1 -u ec2-user --private-key ~/WordPress-terraform-Docker-Prometheus/ansible/key.pem --extra-vars IP_ADDRESS=${aws_instance.ec2.public_ip} -i ${aws_instance.ec2.public_ip},"
+  command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/cloud/WordPress-terraform-Docker-Prometheus/ansible/docker.yml --tags docker1,prometheus1,nginx1,nodeextender1,wordpress1 -u ec2-user --private-key ~/WordPress-terraform-Docker-Prometheus/ansible/key.pem --extra-vars IP_ADDRESS=${aws_instance.ec2.public_ip} -i ${aws_instance.ec2.public_ip},"
 
   }
 
 }
 
-resource "aws_instance" "ec2-WordPress" {
-  ami                    = "ami-6d263d09"
-  instance_type          = "t2.micro"
+//resource "aws_instance" "ec2-WordPress" {
+  //ami                    = "ami-6d263d09"
+  //instance_type          = "t2.micro"
   //user_data              = "${data.template_cloudinit_config.cloudinit-example.rendered}"
-  vpc_security_group_ids = ["${aws_security_group.wordpress.id}"]
-  key_name               = "key"
+  //vpc_security_group_ids = ["${aws_security_group.wordpress.id}"]
+  //key_name               = "key"
 
-  tags {
-    Name = "WordPress"
-  }
+  //tags {
+    //Name = "WordPress"
+  //}
 
-  root_block_device {
-    volume_size = "50"
- }
+  //root_block_device {
+    //volume_size = "50"
+ //}
 
-  provisioner "local-exec" {
-  command = "sleep 100"
-  }
-  provisioner "local-exec" {
-  command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/cloud/WordPress-terraform-Docker-Prometheus/ansible/docker.yml --tags docker1,wordpress1,nodeextender1 -u ec2-user --private-key ~/WordPress-terraform-Docker-Prometheus/ansible/key.pem -i ${aws_instance.ec2-WordPress.public_ip},"
+  //provisioner "local-exec" {
+  //command = "sleep 100"
+  //}
+  //provisioner "local-exec" {
+  //command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /home/cloud/WordPress-terraform-Docker-Prometheus/ansible/docker.yml --tags docker1,wordpress1,nodeextender1 -u ec2-user --private-key ~/WordPress-terraform-Docker-Prometheus/ansible/key.pem -i ${aws_instance.ec2-WordPress.public_ip},"
 
-  }
-}
+  //}
+//}
 output "ip" {
   value = "${aws_instance.ec2.*.public_ip}"
 }
-output "ip2" {
-  value = "${aws_instance.ec2-WordPress.public_ip}"
-}
+//output "ip2" {
+  //value = "${aws_instance.ec2-WordPress.public_ip}"
+//}
 resource "aws_key_pair" "terraform_ec2_key" {
   key_name   = "key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBa1bJ/98f5DgaWrZOqJXzlSDm2Yjj7ahbiC/wXEqOevw6IpKGOv09xaD2fqkQGmnoBOXBuyN9jK5eFoz25kzAoKvsFjOLJ5qf5QY+NdgSw2mwsG0Tb/PNRlbltv533xRFx4mzMoEEqs/QjnA7987j5TWxEVASoI5rNi9tq4twFAohTAnbQrOl8A/2/ZPU3y7rtFWZf6SovaTMfC8Cns8XMEB5tBX79gZZOvDzhaBbpUjOqJvOg2tN73pqEyh8lrXPin+18KPHEbL3WTNdKfwr3Ov8kveanyFpel+vQnjaoVaZL5BlARPdRXFjkQLWtp9THNbIDpqXbhn9IjTLINFH cloud@rancher"
