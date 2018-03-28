798ad81eae4a99f725dda85639836c5ef5cfb87c
Omar Ewedah <oewedah.ext@orange.com>
2018-03-28 12:44:49 +0200

    remove self=true from aws.tf

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -32,8 +32,8 @@ resource "aws_security_group" "monitoring" {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
-    self        = true
-    //idr_blocks = ["0.0.0.0/0"]
+    //self        = true
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
